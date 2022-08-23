2d98c1c7adb1f1bce4a28e334cf90d4caaa7a976
Jeremy Jerez <110129245+JeremyJerez@users.noreply.github.com>
2022-08-23 17:02:28 -0500

    Create plates.py

---
+++ plates.py
@@ -0,0 +1,14 @@
+# Python_Intro
+# Problem Set 2
+#A series of exercises for CS50 hands-on projects
+"""
+This one it's my aproach on the "Vanity Plates" problem
+"""
+x = input("camel case: ")
+print("snake_case: ", end="")
+for i in x:
+    if i.isupper():
+        print("_" + i.lower(), end="")
+    else:
+        print(i, end="")
+print()
